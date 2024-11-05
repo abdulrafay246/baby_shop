d2c037597a332c9b64ea4c72304e893ff88b3c78
exam <exam@Aptechnk1.edu>
2024-11-05 18:04:33 +0500

    Laiba

--- lib/Colors/customcolors.dart
+++ lib/Colors/customcolors.dart
@@ -2,4 +2,9 @@ import 'dart:ui';
 
 class CustomColors {
   static Color purple = Color(0xff9C43F0);
+  static Color textField = Color.fromARGB(249, 85, 85, 85);
+  static Color textlightgrey = Color(0xff555555);
+  static Color black = Color(0x332B3A);
+  static Color appBar = Color(0x000000);
+  
 }
