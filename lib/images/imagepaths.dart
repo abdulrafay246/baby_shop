cf1dc95ebd917d8da95166478d401c5564ebcafa
abdulrafay246 <exam@Aptechnk1.edu>
2024-11-12 18:25:58 +0500

    rafay

--- lib/images/imagepaths.dart
+++ lib/images/imagepaths.dart
@@ -1,5 +1,30 @@
 class ImagePath {
-  static String childImage = "assets/images/slideimage.jpg";
-  static String blue_jumpsuit = "assets/Images/blue_jumpsuit.jpg";
-  static String pink_winter_jacket = "assets/Images/pink_winter_jacket.jpg";
+  static String childImage = "assts/images/b1.jpeg";
+  static String childImage1 = "assts/images/b2.jpeg";
+  static String childImage2 = "assts/images/frock1.jpeg";
+  static String childImage3 = "assts/images/frock2.jpeg";
+  static String childImage4 = "assts/images/frock3.jpeg";
+  static String childImage5 = "assts/images/frock4.jpeg";
+  static String childImage6 = "assts/images/frock5.jpeg";
+  static String childImage7 = "assts/images/frock6.jpeg";
+  static String childImage8 = "assts/images/frock7.jpeg";
+  static String childImage9 = "assts/images/g1.png";
+  static String childImage10 = "assts/images/n1.jpeg";
+  static String childImage11 = "assts/images/n2.jpeg";
+  static String childImage12 = "assts/images/n3.jpeg";
+  static String childImage13 = "assts/images/n4.jpeg";
+  static String childImage14 = "assts/images/sweater1.jpeg";
+  static String childImage15 = "assts/images/sweater2.jpeg";
+  static String childImage16 = "assts/images/sweater3.jpeg";
+  static String childImage17 = "assts/images/kid1.png";
+  static String childImage18 = "assts/images/kid2.png";
+  static String childImage19 = "assts/images/s1.jpeg";
+  static String childImage20 = "assts/images/s2.jpeg";
+  static String childImage21 = "assts/images/s3.jpeg";
+  static String childImage22 = "assts/images/s4.jpeg";
+  static String childImage23 = "assts/images/s5.jpeg";
+  static String childImage24 = "assts/images/s6.jpeg";
+  static String childImage25 = "assts/images/s7.jpeg";
+  static String childImage26 = "assts/images/frock8.jpeg";
+
 }
